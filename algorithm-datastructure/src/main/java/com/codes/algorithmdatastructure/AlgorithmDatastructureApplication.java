4eca33cecc8c29cd618d803647026ebd82129f94
leeconnor <leeconnor@126.com>
2018-07-02 15:48:33 +0800

    first commit

---
+++ algorithm-datastructure/src/main/java/com/codes/algorithmdatastructure/AlgorithmDatastructureApplication.java
@@ -0,0 +1,12 @@
+package com.codes.algorithmdatastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmDatastructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmDatastructureApplication.class, args);
+	}
+}
---
+++ algorithm-datastructure/src/test/java/com/codes/algorithmdatastructure/AlgorithmDatastructureApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codes.algorithmdatastructure;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlgorithmDatastructureApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
